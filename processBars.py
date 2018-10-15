9ead52aea694f993c9bee62e1bee4e5dd4e30b1c
Keith Johnson <11054821+keithj3@users.noreply.github.com>
2018-10-15 15:52:40 -0500

    minor edit

--- processBars.py
+++ processBars.py
@@ -6,14 +6,14 @@ from source import barsToScan
 
 DATA_DIR='wordResults/'
 
-common_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 
-            'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 
-            'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 
-            'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 
-            'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 
-            'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 
-            'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 
-            'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 
+common_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
+            'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
+            'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these',
+            'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do',
+            'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while',
+            'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
+            'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
+            'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each',
             'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
             'too', 'very', 's', 't', 'can', 'will', 'just', 'should', 'now', '\'ve', '\'ll', '\'s', 'place', 'go',
             'austin', 'get', 'time', 'bar', 'bars', 'know', '\'re', '\'m', 'would', 'went', 'way', 'could', 'going',
@@ -22,9 +22,9 @@ common_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'yo
 nlp=spacy.load('en')
 
 def get_aspects(x):
-    doc=nlp(x) ## Tokenize and extract grammatical components
+    doc=nlp(x)  # Tokenize and extract grammatical components
     print(type(doc))
-    doc=[i.text for i in doc if i.text not in common_words and i.pos_ in ['NOUN', 'PROPN', 'VERB']] ## Remove common words and retain only nouns
+    doc=[i.text for i in doc if i.text not in common_words and i.pos_ in ['NOUN', 'PROPN', 'VERB']]  # Remove common words and retain only nouns
     print(type(doc))
     doc=list(map(lambda i: i.lower(),doc)) ## Normalize text to lower case
     print(type(doc))
@@ -33,7 +33,7 @@ def get_aspects(x):
     #doc=doc.value_counts().head().index.tolist() ## Get 5 most frequent nouns
     doc = doc.value_counts()
     print(type(doc))
-    return doc 
+    return doc
 
 def getWords():
 	print('p')
@@ -49,10 +49,10 @@ def getWords():
 		tempResults = get_aspects(rev)
 		for k,v in tempResults.items():
 			if v > 4:
-				results[k] = v 
+				results[k] = v
 		for k,v in results.items():
 			print(k,v)
 		resultsFileName = bar.lower() + ' results.json'
 		with open(os.path.join(DATA_DIR, resultsFileName), 'w') as f:
 			json.dump(results, f)
-			
+
